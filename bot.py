d38402957db731a5d113a7be8abbc3ba0e76e14e
raj210809 <145861977+raj210809@users.noreply.github.com>
2024-07-03 15:15:16 +0530

    feat : setting up slackbot

--- bot.py
+++ bot.py
@@ -1,10 +1,30 @@
 import slack
 import os
 from pathlib import Path
+from flask import Flask
 from dotenv import load_dotenv
+from slackeventsapi import SlackEventAdapter
 
 env_path = Path(".") / '.env'
 load_dotenv(dotenv_path=env_path)
 
+app= Flask(__name__)
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-client.chat_postMessage(channel='#slackbo', text='kaise ho broooooo')
+slack_event_adapter = SlackEventAdapter(
+    os.environ['SIGNING_SECRET'], '/slack/events' , app)
+
+botid = client.api_call("auth.test")["user_id"]
+
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event' , {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+    if user_id != botid:
+        client.chat_postMessage(channel=channel_id , text=text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
